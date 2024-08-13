88b78f525203540d22bfa43d6a9cc97d1a112b16
Lais Barreto <lais.barreto@carriers.com.br>
2024-08-13 14:52:20 -0300

    first commit

---
+++ app/faq/page.js
@@ -0,0 +1,15 @@
+import Link from '../../src/components/Link';
+
+export default function Faq() {
+    return(
+        <><div>
+            <h1>
+                Página FAQ
+            </h1>
+        </div><div>
+                <Link href='/'>
+                    Retornar para página inicial
+                </Link>
+            </div></>
+    )
+}
